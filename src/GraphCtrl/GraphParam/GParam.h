4fbfb0ec8ea3d891b7ebee98d67d777aed6b39d6
Chunel <Chunel@foxmail.com>
2026-08-15 14:19:23 +0800

    [bugfix] fix rw lock define

--- src/GraphCtrl/GraphParam/GParam.h
+++ src/GraphCtrl/GraphParam/GParam.h
@@ -18,11 +18,7 @@ CGRAPH_NAMESPACE_BEGIN
 
 class GParam : public GParamObject {
 public:
-#if __cplusplus >= 201703L && _CGRAPH_GPARAM_RWLOCK_ENABLE_
-    std::shared_mutex _param_shared_lock_;    // 用于参数互斥的锁信息
-#else
-    std::recursive_mutex _param_shared_lock_;
-#endif
+    std::recursive_mutex _param_shared_lock_ {};
 
     /**
      * 获取参数的调用栈信息
